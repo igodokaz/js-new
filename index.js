3d6bc5421f9353def906fe77b86b93e55d237c62
JEONG JINBO <igodokaz@gmail.com>
2021-10-30 15:48:04 +0900

    save_1

---
+++ index.js
@@ -0,0 +1,12 @@
+// const title = document.getElementById("title");
+// title.innerHTML = "Hi! From JS";
+// title.style.color = "red";
+// // console.log(title);
+// // console.dir(title);
+// // console.dir(document);
+// document.title = "I own you";
+
+const title = document.querySelector("#title");
+title.innerHTML = "Hi! From JS";
+title.style.color = "red";
+document.title = "I own you";
