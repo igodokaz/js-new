1a8f3d74ed8195688aac28df201e4f0bcd9c710d
JEONG JINBO <igodokaz@gmail.com>
2021-10-30 17:08:13 +0900

    Save_5

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
+// const title = document.querySelector("#title");
+
+// const CLICKED_CLASS = "clicked";
+
+// function handleClick() {
+//   const currentClass = title.className;
+//   if (currentClass !== CLICKED_CLASS) {
+//     title.className = CLICKED_CLASS;
+//   } else {
+//     title.className = "";
+//   }
+// }
+
+// function init() {
+//   title.addEventListener("click", handleClick);
+// }
+// init();
+
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "red";
+const CLICKED_CLASS = "clicked";
+
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+  //   const hasClass = title.classList.contains(CLICKED_CLASS);
+
+  //   if (hasClass) {
+  //     title.classList.remove(CLICKED_CLASS);
+  //   } else {
+  //     title.classList.add(CLICKED_CLASS);
+  //   }
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  // 어플리케이션 초기화
-  title.style.color = BASE_COLOR;
-  //   title.addEventListener("click", handleClick);
-  title.addEventListener("mouseenter", handleClick);
+  title.addEventListener("click", handleClick);
 }
-
 init();
-
-// function handleOffLine() {
-//   console.log("gaga");
-// }
-// function handleOnLine() {
-//   console.log("byebyt");
-// }
-// window.addEventListener("offline", handleOffLine);
-// window.addEventListener("online", handleOnLine);
