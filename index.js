e9f5c877f6b23444871ce3deb6efd6f09f6b4256
JEONG JINBO <igodokaz@gmail.com>
2021-10-30 16:48:52 +0900

    Save_4

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-// prompt("ask something");
+const title = document.querySelector("#title");
 
-// const age = prompt("How old are you?");
-// console.log(age);
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "red";
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
 
-// const age = prompt("How old are you?");
-// if (age > 18 && age < 21) {
-//   console.log("you can drink but you should not");
-// } else if (age > 21) {
-//   console.log("go ahead");
-// } else {
-//   console.log("too young");
+function init() {
+  // 어플리케이션 초기화
+  title.style.color = BASE_COLOR;
+  //   title.addEventListener("click", handleClick);
+  title.addEventListener("mouseenter", handleClick);
+}
+
+init();
+
+// function handleOffLine() {
+//   console.log("gaga");
 // }
+// function handleOnLine() {
+//   console.log("byebyt");
+// }
+// window.addEventListener("offline", handleOffLine);
+// window.addEventListener("online", handleOnLine);
