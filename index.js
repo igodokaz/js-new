30f9460c16521b45f95c47456dc3dbf626aa51d5
JEONG JINBO <igodokaz@gmail.com>
2021-10-30 15:59:43 +0900

    Save_2

--- index.js
+++ index.js
@@ -6,7 +6,22 @@
 // // console.dir(document);
 // document.title = "I own you";
 
+// const title = document.querySelector("#title");
+// title.innerHTML = "Hi! From JS";
+// title.style.color = "red";
+// document.title = "I own you";
+
+// const title = document.querySelector("#title");
+// function handleResize(event) {
+//   //상세내용 호출
+//   console.log(event); //상세내용 호출
+// }
+
+// window.addEventListener("resize", handleResize);
+
 const title = document.querySelector("#title");
-title.innerHTML = "Hi! From JS";
-title.style.color = "red";
-document.title = "I own you";
+function handleClick() {
+  title.style.color = "blue";
+}
+
+window.addEventListener("click", handleClick);
